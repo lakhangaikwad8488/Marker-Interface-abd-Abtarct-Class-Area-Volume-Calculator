f69a3e1d4ef7fc33783a9e6ff3d31e874dcf1a90
lakhangaikwad8488 <70312128+lakhangaikwad8488@users.noreply.github.com>
2020-10-16 12:41:08 +0530

    Update Main.java

--- src/main/java/main/java/yaksha/Main.java
+++ src/main/java/main/java/yaksha/Main.java
@@ -10,47 +10,17 @@ public static void main(String[] args) {
 	for(int i=0;i<5;i++) {
 	System.out.println("Enter the type of shape [Rectangle/Triangle/Cube/Sphere]");
 	String shape=sc.next();
-	if(shape.equals("Rectangle")) {
-		Rectangle r=new Rectangle();
-		double length=sc.nextDouble();
-		double width=sc.nextDouble();
-		r.setLength(length);
-		r.setWidth(width);
-		s[i]=r;
-
- 	}else if(shape.equals("Triangle")) {
- 		Triangle t=new Triangle();
-		double base=sc.nextDouble();
-		double height=sc.nextDouble();
-		 t.setBase(base);
-		 t.setHeight(height);
-		s[i]=t;
-		
-	}else if(shape.equals("Cube")) {
-		Cube c=new Cube();
-		double length=sc.nextDouble();
-		double width=sc.nextDouble();
-		double height=sc.nextDouble();
-		 c.setLength(length);
-		 c.setWidth(width);
-		c.setHeight(height);
-		s[i]=c;
-		
-	}else if(shape.equals("Sphere")) {
-		Sphere sp=new Sphere();
-		double radius=sc.nextDouble();
-		 sp.setRadius(radius);
-		s[i]=sp;
-	}
+	/* Write code here. Check shape is Rectangle or triangle or cube or sphere and create object of respected shape
+	and accept parameters and set value to object using setter methods
+	
+	add multiple object of [Rectangle/Triangle/Cube/Sphere] in array after setting value of object 
+	*/
+		 
 	
 	}
 	for(int i=0;i<5;i++) {
 		
-		double ar=s[i].area();
-		System.out.println("Area :"+ar);
-		if(s[i] instanceof Spatial) {
-			double vol=s[i].volume();
-			System.out.println("Volume :"+vol);
+		/* find area and volume of Rectangle/Triangle/Cube/Sphere  by accessing array of object .For Rectangle and Triangle class the method volume should return -1./*
 		}
 	}
 	
